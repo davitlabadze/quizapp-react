4f4fd3fcd010b73c68f5b80b3c3c13c702c61587
davitlabadze <labadzedato18@gmail.com>
2023-09-30 14:56:10 +0400

    chore: wip vercel 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
-import ErrorPage from './error-page';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 import Root from "./routes/root";
 import ReactRouter from "./routes/react";
 import Vue from "./routes/vue";
 import Angular from "./routes/angular";
-import './index.css'
+import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -27,8 +27,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
