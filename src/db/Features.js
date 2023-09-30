83a429fae6c60ed23398ef81baa4bb1011b1ce04
davitlabadze <labadzedato18@gmail.com>
2023-09-30 14:38:37 +0400

    chore(feature): change name

--- src/db/Features.js
+++ src/db/Features.js
@@ -2,7 +2,7 @@ import { BiLogoReact, BiLogoAngular } from "react-icons/bi";
 import { FaVuejs } from "react-icons/fa";
 const Features = [
   {
-    name: "React Quiz",
+    name: "React",
     description:
       "React can change how you think about the designs you look at and the apps you build. When you build a user interface with React, you will first break it apart into pieces called components. Then, you will describe the different visual states for each of your components. Finally, you will connect your components together so that the data flows through them.",
     href: "react",
@@ -10,7 +10,7 @@ const Features = [
     icon: BiLogoReact,
   },
   {
-    name: "Vue Quiz",
+    name: "Vue",
     description:
       "We believe there is no 'one size fits all' story for the web. This is why Vue is designed to be flexible and incrementally adoptable. Depending on your use case, Vue can be used in different ways to strike the optimal balance between stack complexity, developer experience and end performance.",
     href: "vue",
@@ -18,7 +18,7 @@ const Features = [
     icon: FaVuejs,
   },
   {
-    name: "Angular Quiz",
+    name: "Angular",
     description:
       "you're taking advantage of a platform that can scale from single-developer projects to enterprise-level applications. Angular is designed to make updating as straightforward as possible, so take advantage of the latest developments with minimal effort. Best of all, the Angular ecosystem consists of a diverse group of over 1.7 million developers, library authors, and content creators",
     href: "angular",
