139177823ae15cd3d2bbdbdd3cafa083a52d3fc3
davitlabadze <labadzedato18@gmail.com>
2023-09-30 14:58:02 +0400

    wip: vercel 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./error-page";
+import ErrorPage from "./404";
 import Root from "./routes/root";
 import ReactRouter from "./routes/react";
 import Vue from "./routes/vue";
